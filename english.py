c85746e557022781fc60aa6408ebc3cf98268e97
jebrinton <122479901+jebrinton@users.noreply.github.com>
2024-10-01 19:07:15 -0400

    Added test_ngram

--- english.py
+++ english.py
@@ -34,6 +34,28 @@ def dev_ngram(model):
 
     return num_correct, total
 
+def test_ngram(model):
+    test_path = "data/english/test"
+    test_data = load_chars_from_file(test_path)
+
+    # remember: the errors down below are expected (I think)
+
+    num_correct: int = 0
+    total: int = 0
+    for dev_line in test_data:
+
+        # print(dev_line)
+
+        q = model.start()
+        for c_input, c_actual in zip([utils.START_TOKEN] + dev_line, dev_line + [utils.END_TOKEN]):
+            q, p = model.step(q, c_input)
+            c_predicted = max(p.keys(), key=lambda k: p[k])
+            num_correct += int(c_predicted == c_actual)
+            total += 1
+
+    return num_correct, total
+
+
 if __name__ == "__main__":
     my_model = train_ngram()
     dev_ngram(my_model)

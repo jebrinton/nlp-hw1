b06674283d3403d990569248847a13e2b41b0b2a
jebrinton <122479901+jebrinton@users.noreply.github.com>
2024-10-01 22:42:25 -0400

    Checking dev and test on mandarin.py

--- mandarin.py
+++ mandarin.py
@@ -10,30 +10,26 @@ from utils import read_mono
 def train_model():
     return CharPredictor(2, "data/mandarin/charmap", "data/mandarin/train.han")
 
-def dev_model(model):
-    dev_pin = load_chars_from_file("data/mandarin/dev.pin")
-    dev_han = load_chars_from_file("data/mandarin/dev.han")
-
-    # remember: the errors down below are expected (I think)
+def dev_model(m):
+    pin_data = read_mono("./data/mandarin/dev.pin", delim=" ")
+    han_data: Sequence[Sequence[str]] = read_mono("./data/mandarin/dev.han", delim='')
 
     num_correct: int = 0
     total: int = 0
+    for pin_line, han_line in zip(pin_data, han_data):
 
-    i = 0
-    for pin, han in zip(dev_pin, dev_han):
-        if (i % 1000 == 0):
-            print(f"p/h   {pin} - {han}")
-        i += 1
+        q = m.start()
 
-        q = model.start()
+        for c_input, c_actual in zip(pin_line[1:-1], han_line[1:-1]):
 
-        for c_input, c_actual in zip(pin[1:-1], han[1:-1]):
-            q, p = model.step(q, c_input)
-            q, _ = model.model.step(q, c_actual)
+            q, p = m.step(q, c_input)
+            q, _ = m.model.step(q, c_actual)
             c_predicted = max(p.keys(), key=lambda k: p[k])
+
             num_correct += int(c_predicted == c_actual)
             total += 1
 
+    print(num_correct/total)
     return num_correct, total
 
 def test_model(m):
@@ -48,7 +44,7 @@ def test_model(m):
 
         for c_input, c_actual in zip(pin_line[1:-1], han_line[1:-1]):
             q, p = m.step(q, c_input)
-            q, _ = m.lm.step(q, c_actual)
+            q, _ = m.model.step(q, c_actual)
 
             c_predicted = max(p.keys(), key=lambda k: p[k])
 
